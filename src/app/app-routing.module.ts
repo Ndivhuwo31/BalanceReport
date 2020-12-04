64565c9e9179bcdad297d38ab9a93d00116f87fc
Ndivhuwo31 <ndivhuwoannie@gmail.com>
2020-12-04 07:56:53 +0200

    fghjkl;g

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExpencesComponent } from './components/expences/expences.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
+import { IncomeComponent } from './components/income/income.component';
+
+
+// this constant default routes to Home, and also send all unknown path to Home
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'expense', component: ExpencesComponent },
+  { path: 'income', component:IncomeComponent },
+
+  // default route
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TestComponent } from './components/test/test.component';
+import { ExpencesComponent } from './components/expences/expences.component';
+import { IncomeComponent } from './components/income/income.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TestComponent,
+    ExpencesComponent,
+    IncomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
