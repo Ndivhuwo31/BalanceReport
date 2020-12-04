1641b513d42dc41756b19722705702ff46b25ef7
Ndivhuwo31 <ndivhuwoannie@gmail.com>
2020-12-04 11:35:31 +0200

    can now console data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+annie:any =[];
   constructor() { }
 
   ngOnInit(): void {
