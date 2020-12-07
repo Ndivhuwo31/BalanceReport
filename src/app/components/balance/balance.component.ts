bb238d91d2edfb574b0ce85329ac97943f26461f
Ndivhuwo31 <ndivhuwoannie@gmail.com>
2020-12-07 11:49:51 +0200

    working

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balance.component.scss']
 })
 export class BalanceComponent implements OnInit {
-
+totalBalance :number =0;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    let transactions: any[] = JSON.parse(localStorage.getItem('transactions'));
 
+    for (let i = 0; i < transactions.length; i++) {
+      console.log(transactions[i].transactionType)
+
+      if (transactions[i].transactionType == "Expences") {
+      this.totalBalance = this.totalBalance - transactions[i].expenseAmount
+      console.log(this.totalBalance)
+    }
+    else {
+      this.totalBalance = this.totalBalance + transactions[i].expenseAmount
+    }
+    }
+  }
 }
